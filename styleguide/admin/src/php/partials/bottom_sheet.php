a67a36da7100b1a9f4789af04a51c1d236bac5e9
Leandro Rezende <lbrezende@gmail.com>
2015-09-03 17:45:03 -0300

    Gulp refatorado, limpeza do template realizada

--- styleguide/admin/src/php/partials/bottom_sheet.php
+++
@@ -1,3 +0,0 @@
-<div id="bottom_sheet">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum cupiditate enim nemo odit voluptate? Animi aspernatur assumenda dignissimos dolore eius et hic, laudantium molestiae nostrum porro reprehenderit sit, veniam voluptatibus.
-</div>

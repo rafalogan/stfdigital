a0beb9a422fa9490f2894b15419519f3e6d09168
Rafael.Alencar <rafaelesdras@gmail.com>
2015-09-03 11:58:31 -0300

    Integração inicial com o Flyway.

--- src/main/java/br/jus/stf/plataforma/ApplicationContextInitializer.java
+++ src/main/java/br/jus/stf/plataforma/ApplicationContextInitializer.java
@@ -1,13 +1,19 @@
 package br.jus.stf.plataforma;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 /**
@@ -19,7 +25,7 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @SpringBootApplication
 @ComponentScan("br.jus.stf")
 @EntityScan(basePackages = 
-	{"br.jus.stf.autuacao.domain.model", "br.jus.stf.generico.domain.model", "br.jus.stf.shared.domain.model"})
+	{"br.jus.stf.autuacao.domain.model", "br.jus.stf.generico.domain.model", "br.jus.stf.plataforma.domain.model", "br.jus.stf.shared.domain.model"})
 public class ApplicationContextInitializer {
 
     public static void main(String[] args) {
@@ -35,10 +41,43 @@ public class ApplicationContextInitializer {
 	public DataSource memoryDataSourceServidor() throws Exception {
 		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 		dataSource.setDriverClass(org.h2.Driver.class);
-		dataSource.setUrl("jdbc:h2:~/stfdigital;AUTO_SERVER=TRUE");
+		dataSource.setUrl("jdbc:h2:~/test;MODE=Oracle;AUTO_SERVER=TRUE;DB_CLOSE_DELAY=-1;");
 		dataSource.setUsername("sa");
 		dataSource.setPassword("");
 		return dataSource;
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws Exception {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(memoryDataSourceServidor());
+
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		
+		Properties properties = new Properties();
+	    properties.setProperty("hibernate.hbm2ddl.auto", "validate");
+	    properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+	    properties.setProperty("show_sql", "true");
+	    properties.setProperty("format_sql", "true");
+		em.setJpaProperties(properties);
+
+		return em;
+	}
+	
+	@Bean(name = "flyway", initMethod="migrate")
+	public Flyway flywaySchemaVersion() throws Exception {
+		Flyway flyway = new Flyway();
+		
+		flyway.setDataSource(memoryDataSourceServidor());
+		flyway.setBaselineOnMigrate(true);
+		flyway.setBaselineVersion("0");
+		
+		if(!flyway.isValidateOnMigrate()) {
+			flyway.baseline();
+		}
+		
+		return flyway;
 	}
     
 }

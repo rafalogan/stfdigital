05ce2626a9bd182a6563e8fb3ffc57640920e33e
Lucas.Rodrigues <Lucas.Rodrigues@stf.gov.br>
2015-09-10 19:51:50 -0300

    Retirado profile de desenvolvimento da persistência

--- src/main/java/br/jus/stf/plataforma/persistence/PersistenceMemoryConfiguration.java
+++ src/main/java/br/jus/stf/plataforma/persistence/PersistenceMemoryConfiguration.java
@@ -4,20 +4,17 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-
-import br.jus.stf.plataforma.settings.Profiles;
 
 /**
  * @author Lucas.Rodrigues
  *
  */
 @Configuration
-@Profile(Profiles.DESENVOLVIMENTO)
+//@Profile(Profiles.DESENVOLVIMENTO)
 public class PersistenceMemoryConfiguration {
 	
 	@Bean(name = "dataSource")

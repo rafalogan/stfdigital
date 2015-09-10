055247d459a4508245a45ccee461710c489e6094
viniciusksilva <viniciusksilva@gmail.com>
2015-09-10 20:40:01 -0300

    Criada a tela de detalhe das petições e atualizados os aquivos do teste unitário

---
+++ src/main/resources/static/application/test/unit/autuacao/registro.controller.spec.js
@@ -0,0 +1,38 @@
+/**
+ * @author Rodrigo Barreiros
+ * 
+ * @since 1.00
+ * @since 01.09.2015
+ */ 
+/* jshint undef:false*/
+(function() {
+	'use strict';
+
+	describe('Registro Controller', function() {
+		var fakeData = [];
+		var stateParams = [];
+		var controller;
+		var scope;
+
+		beforeEach(module('appDev'));
+		
+		beforeEach(inject(function($rootScope, $controller, $httpBackend, $window, $log, properties, TipoRecebimentoService) {
+			$window.sessionStorage.papel = JSON.stringify('recebedor');
+			
+			$httpBackend.expectGET(properties.apiUrl + '/tiporecebimentos').respond([{sigla : '2', nome: 'Sedex'}, {sigla : '3', nome: 'Malote'}]);
+			$httpBackend.expectGET(properties.apiUrl + '/tarefas').respond([{}]);
+			
+			stateParams = {idTarefa: 4};
+			
+			
+			controller = $controller('RegistroPeticaoFisicaController', {
+				$stateParams : stateParams,
+				$log: $log
+			});
+			$httpBackend.flush();
+		}));
+		it ('Deveria carregar a lista de forma de envio do controlador', function(){
+			expect(controller.tipoRecebimentos.length).toEqual(2);
+		})
+	});
+})();

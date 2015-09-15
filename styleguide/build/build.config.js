7b140fd14c502932608bd45f7e278201ea7641bf
Leandro Rezende <lbrezende@gmail.com>
2015-09-15 19:22:58 -0300

    Reorganizando diretorios. Nao iniciado refatoracao em Jade. Nao corrigido gulp para gerar o dist

--- styleguide/build/build.config.js
+++ styleguide/build/build.config.js
@@ -2,7 +2,7 @@
  * Define o conjunto de variáveis que serão usadas pelo gulp ao executar
  * as tarefas de construção.
  * 
- * @author Rodrigo Barreiros
+ * @author Leandro Rezende
  * 
  * @since 06.07.2015
  * @since 1.0.0
@@ -10,15 +10,17 @@
 'use strict';
 
 module.exports = {
-	proxy : 'localhost/stfdigital/styleguide/tpl',
+	proxy : 'localhost/stfdigital/styleguide/dist',
 	assets : ['src/assets/js/**/*', 'src/assets/fonts/**/*'],
 	images : 'src/assets/images/**/*',
 	sass : 'src/assets/sass/**/*',
-	sasscompiled : 'tpl/assets-stf/css',
-	dist : 'tpl/',
-    html : 'tpl/*.html',
-    jsvendor : 'tpl/assets/js/**/*.js',
-    css : 'tpl/assets-stf/css/**/*.css',
-    js : 'tpl/assets-stf/js/**/*.js',  
-    imagesstf : 'tpl/assets-stf/images/**/*'   
+	jade : 'src/assets/jade/**/*',	
+	sasscompiled : 'dist/assets-stf/css',
+	htmlcompiled : 'dist/assets-stf/css',	
+	dist : 'dist/',
+    html : 'dist/*.html',
+    jsvendor : 'dist/assets/js/**/*.js',
+    css : 'dist/assets-stf/css/**/*.css',
+    js : 'dist/assets-stf/js/**/*.js',  
+    imagesstf : 'dist/assets-stf/images/**/*'   
 };

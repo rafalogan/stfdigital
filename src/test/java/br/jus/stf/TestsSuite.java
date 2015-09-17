ba59dbca91f257eff7622201ce27b3906b9a3bc3
lucas <lucas.rodrigues@stf.jus.br>
2015-09-16 21:22:09 -0300

    Classe de teste excluída

--- src/test/java/br/jus/stf/TestsSuite.java
+++ src/test/java/br/jus/stf/TestsSuite.java
@@ -9,12 +9,11 @@ import br.jus.stf.autuacao.application.PeticaoApplicationServiceUnitTests;
 import br.jus.stf.autuacao.application.PeticaoIntegrationTests;
 import br.jus.stf.autuacao.domain.ClasseProcessualServiceUnitTests;
 import br.jus.stf.autuacao.domain.MinistroServiceUnitTests;
-import br.jus.stf.autuacao.interfaces.AutuacaoOriginariosIntegrationTests;
 import br.jus.stf.generico.interfaces.DocumentoIntegrationTests;
 import br.jus.stf.shared.infra.action.AcoesIntegrationTests;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AutuacaoOriginariosIntegrationTests.class, AcoesIntegrationTests.class,
+@SuiteClasses({ AcoesIntegrationTests.class,
 	DocumentoIntegrationTests.class, PeticaoApplicationServiceUnitTests.class,
 	PeticaoIntegrationTests.class, ClasseProcessualServiceUnitTests.class,
 	MinistroServiceUnitTests.class, PeticaoApplicationEventIntegrationTests.class})

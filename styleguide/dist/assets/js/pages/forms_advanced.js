d96df651e7bb6b88d8444818473588a597fc2de4
Leandro Rezende <lbrezende@gmail.com>
2015-09-21 16:02:49 -0300

    incluíndo alguns estados ddiferentes

--- styleguide/dist/assets/js/pages/forms_advanced.js
+++ styleguide/dist/assets/js/pages/forms_advanced.js
@@ -12,6 +12,95 @@ altair_form_adv = {
         var REGEX_EMAIL = '([a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@' +
             '(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)';
 
+        $('#selec_adv_3').selectize({
+            persist: false,
+            maxItems: null,
+            valueField: 'email',
+            labelField: 'name',
+            searchField: ['name', 'email'],
+            options: [
+                {email: 'Ricardo Lewandowski', name: 'Ministro'},
+                {email: 'Cármen Lúcia', name: 'Ministro'},
+                {email: 'Celso de Mello', name: 'Ministro'},
+                {email: 'Marco Aurélio', name: 'Ministro'},
+                {email: 'Gilmar Mendes', name: 'Ministro'},
+                {email: 'Dias Toffoli', name: 'Ministro'},
+                {email: 'Luiz Fux', name: 'Ministro'},
+                {email: 'Rosa Weber', name: 'Ministro'},
+                {email: 'Teori Zavascki', name: 'Ministro'},
+                {email: 'Roberto Barroso', name: 'Ministro'},
+                {email: 'Edson Fachin', name: 'Ministro'}
+
+            ],
+            render: {
+                item: function(item, escape) {
+                    return '<div>' +
+                        (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
+                        (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
+                        '</div>';
+                },
+                option: function(item, escape) {
+                    var label = item.name || item.email;
+                    var caption = item.name ? item.email : null;
+                    return '<div>' +
+                        '<span class="label">' + escape(label) + ' - </span>' +
+                        (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
+                        '</div>';
+                }
+            },
+            createFilter: function(input) {
+                var match, regex;
+
+                // email@address.com
+                regex = new RegExp('^' + REGEX_EMAIL + '$', 'i');
+                match = input.match(regex);
+                if (match) return !this.options.hasOwnProperty(match[0]);
+
+                // name <email@address.com>
+                regex = new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i');
+                match = input.match(regex);
+                if (match) return !this.options.hasOwnProperty(match[2]);
+
+                return false;
+            },
+            create: function(input) {
+                if ((new RegExp('^' + REGEX_EMAIL + '$', 'i')).test(input)) {
+                    return {email: input};
+                }
+                var match = input.match(new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i'));
+                if (match) {
+                    return {
+                        email : match[2],
+                        name  : $.trim(match[1])
+                    };
+                }
+                alert('Invalid email address.');
+                return false;
+            },
+            onDropdownOpen: function($dropdown) {
+                $dropdown
+                    .hide()
+                    .velocity('slideDown', {
+                        begin: function() {
+                            $dropdown.css({'margin-top':'0'})
+                        },
+                        duration: 200,
+                        easing: easing_swiftOut
+                    })
+            },
+            onDropdownClose: function($dropdown) {
+                $dropdown
+                    .show()
+                    .velocity('slideUp', {
+                        complete: function() {
+                            $dropdown.css({'margin-top':''})
+                        },
+                        duration: 200,
+                        easing: easing_swiftOut
+                    })
+            }
+        });
+
         $('#selec_adv_2').selectize({
             persist: false,
             maxItems: null,

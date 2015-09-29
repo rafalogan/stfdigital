054e471d26c3912b7806f0e414ea7df1cf1e4f1e
Rodrigo Barreiros <rodrigo.barreiros@stf.jus.br>
2015-09-29 19:15:02 -0300

    Conclusão da Issue PLATAFORMA-227: Consulta de Processos

--- src/main/java/br/jus/stf/processamentoinicial/distribuicao/interfaces/facade/ProcessoServiceFacade.java
+++ src/main/java/br/jus/stf/processamentoinicial/distribuicao/interfaces/facade/ProcessoServiceFacade.java
@@ -9,10 +9,12 @@ import br.jus.stf.processamentoinicial.autuacao.domain.model.Peticao;
 import br.jus.stf.processamentoinicial.autuacao.domain.model.PeticaoRepository;
 import br.jus.stf.processamentoinicial.distribuicao.application.ProcessoApplicationService;
 import br.jus.stf.processamentoinicial.distribuicao.domain.model.Processo;
+import br.jus.stf.processamentoinicial.distribuicao.domain.model.ProcessoRepository;
 import br.jus.stf.processamentoinicial.distribuicao.interfaces.dto.ProcessoDto;
 import br.jus.stf.processamentoinicial.distribuicao.interfaces.dto.ProcessoDtoAssembler;
 import br.jus.stf.shared.MinistroId;
 import br.jus.stf.shared.PeticaoId;
+import br.jus.stf.shared.ProcessoId;
 
 @Component
 public class ProcessoServiceFacade {
@@ -22,13 +24,31 @@ public class ProcessoServiceFacade {
 	
 	@Autowired
 	private ProcessoDtoAssembler processoDtoAssembler;
+	
+	@Autowired
+	private ProcessoRepository processoRepository;
 
 	// TODO: Remover essa dependência com o módulo de petições
 	@Autowired
 	private PeticaoRepository peticaoRepository;
 
 	/**
+	 * Consulta um processo judicial, dado o seu identificador primário
+	 * 
+	 * @param id o código de identificação do processo
+	 * @return dto o DTO com as informações de retorno do processo
+	 */
+	public ProcessoDto consultar(Long id){
+		ProcessoId processoId = new ProcessoId(id);
+		
+		Processo processo = Optional.ofNullable(processoRepository.findOne(processoId)).orElseThrow(IllegalArgumentException::new);
+		
+		return processoDtoAssembler.toDto(processo);
+	}
+
+	/**
 	 * Distribui um processo para um ministro relator.
+	 * 
 	 * @param peticaoId Id da petição.
 	 * @param ministroId Id do Ministro Relator.
 	 */

5eee86ce8c680a44993aa719920eb9fce2808b2a
Tomas.Godoi <tomas.godoi@stf.jus.br>
2015-10-05 15:45:26 -0300

    - Adicionando os arquivos do template de preautuar.

---
+++ src/main/resources/static/application/autuacao/preautuacao/dashlets/peticoes-preautuar.controller.js
@@ -0,0 +1,14 @@
+/**
+ * @author Tomas.Godoi
+ * 
+ * @since 1.0.0
+ */ 
+(function() {
+	'use strict';
+	
+	angular.autuacao.controller('MinhasPeticoesParaAutuarDashletController', ['$scope', function($scope) {
+		$scope.titulo = 'Dashlet Minhas Petições para Autuar';
+	}]);
+
+})();
+

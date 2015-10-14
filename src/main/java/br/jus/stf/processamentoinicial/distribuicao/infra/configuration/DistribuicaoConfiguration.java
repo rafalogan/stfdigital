79049ca31aaad4329a1c6357dfe2f16b157aa24e
Lucas.Rodrigues <Lucas.Rodrigues@stf.gov.br>
2015-10-14 20:08:24 -0300

    Alteração de carregamento de configuração do elasticsearch

--- src/main/java/br/jus/stf/processamentoinicial/distribuicao/infra/configuration/DistribuicaoConfiguration.java
+++ src/main/java/br/jus/stf/processamentoinicial/distribuicao/infra/configuration/DistribuicaoConfiguration.java
@@ -1,11 +1,13 @@
 package br.jus.stf.processamentoinicial.distribuicao.infra.configuration;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.annotation.PostConstruct;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.StringUtils;
 
 import br.jus.stf.processamentoinicial.distribuicao.infra.IndexadorRestAdapter;
@@ -17,17 +19,27 @@ import br.jus.stf.processamentoinicial.distribuicao.infra.IndexadorRestAdapter;
 @Configuration
 public class DistribuicaoConfiguration {
 	
+	private static final String DISTRIBUICAO_RESOURCE = "/indices/processamentoinicial/autuacao.json";
+	
 	@Autowired
 	private IndexadorRestAdapter indexadorRestAdapter;
 	
 	@PostConstruct
 	private void configure() throws Exception {
-				
-		ClassPathResource resource = new ClassPathResource("/indices/processamentoinicial/distribuicao.json");
-		String configuracao = FileUtils.readFileToString(resource.getFile());
-		configuracao = StringUtils.trimAllWhitespace(configuracao);
-			
+		String configuracao = readConfiguration(DISTRIBUICAO_RESOURCE);
 		indexadorRestAdapter.criarIndice("distribuicao", configuracao);
+	}
+	
+	/**
+	 * Retorna uma string do conteúdo do arquivo
+	 * 
+	 * @param location
+	 * @return
+	 * @throws IOException 
+	 */
+	private String readConfiguration(String location) throws IOException {
+		InputStream input = getClass().getResourceAsStream(location);
+		return StringUtils.trimAllWhitespace(IOUtils.toString(input));
 	}
 	
 }

df9f78b44fcc07c491770392ce59d0c3a7447ce3
Tomas.Godoi <tomas.godoi@stf.jus.br>
2015-10-23 18:44:04 -0200

    - Criando o índice do Kibana com configuração dynamic true.

---
+++ src/main/java/br/jus/stf/plataforma/pesquisas/infra/configuration/KibanaConfiguration.java
@@ -0,0 +1,45 @@
+package br.jus.stf.plataforma.pesquisas.infra.configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import br.jus.stf.processamentoinicial.autuacao.infra.IndexadorRestAdapter;
+
+/**
+ * @author Tomas.Godoi
+ *
+ */
+@Configuration
+public class KibanaConfiguration {
+	
+	private static final String KIBANA_RESOURCE = "/indices/plataforma/kibana.json";
+	
+	@Autowired
+	private IndexadorRestAdapter indexadorRestAdapter;
+	
+	@PostConstruct
+	private void configure() throws Exception {
+		String configuracao = readConfiguration(KIBANA_RESOURCE);
+		indexadorRestAdapter.criarIndice(".kibana", configuracao);
+	}
+	
+	/**
+	 * Retorna uma string do conteúdo do arquivo
+	 * 
+	 * @param location
+	 * @return
+	 * @throws IOException 
+	 */
+	private String readConfiguration(String location) throws IOException {
+		InputStream input = getClass().getResourceAsStream(location);
+		return StringUtils.trimAllWhitespace(IOUtils.toString(input));
+	}
+	
+}
